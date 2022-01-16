48ddaeec54ba1efdc13def026036e3d8b30affbe
Sandy McCabe <sandytech13@gmail.com>
2022-01-16 15:43:38 -0600

    updated readme and comments, final push

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# open-weather
+# Weather Dashboard
+
+
+## Description
+```
+An application that will display the current and forecasted weather for the city of your choosing.  It will also save your history of recently searched cities.
+
+```
+
+## Website
+
+The website address is https://sandymccabe.github.io/weather-dashboard/
+
+![alt text](assets\image\weather.JPG "Weather Dashboard")
+
+
+## Technologies 
+Technologies used in the making of this site include HTML, CSS, DOM Manipulation and JavaScript.
+
+## License
+There is currently no license applied to this website.
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,8 @@
 let APIKey = "691e962c4a728ff0ee3b3b17e898d64f";
 let locations = [];
 
-// Here we are building the URL we need to query the One call weather API to retrieve the UV Index.  
-//This API requires Geographical coordinates that we get from the 5 day API so we can retirive the 
-//UV Index for the current day and the next 5 days and store them in the uvInxex array
 function getWeatherData(lat, lon, city) {
-
+    // build the URL string to OpenWeather
     var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=,minutely,hourly,alerts&appid=" + APIKey;
 
     //call to the OpenWeatherMap API
@@ -14,73 +11,63 @@ function getWeatherData(lat, lon, city) {
         url: queryURL,
         method: "GET"
     })
-        // We store all of the retrieved data inside of an object called "response"
+        // store the data from the API call
         .then(function (response) {
             showWeatherData(response, city);
-        });           
- };
-
+        });
+};
 
 function loadWeatherCity(city, isClicked) {
-    
+
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + ",us&appid=" + APIKey;
     var weatherContainer = $("#weatherContainer");
 
-    // Here we run our AJAX call to the OpenWeatherMap API
     $.ajax({
         url: queryURL,
         method: "GET"
     })
-        // We store all of the retrieved data inside of an object called "response"
         .then(function (response) {
-
-            console.log(response);
-
-            if (!isClicked)
-            {
-                saveLocations(response);  //save the city to local storage
+            // console.log(response);
+            if (!isClicked) {
+                saveLocations(response);
                 renderLocations();
             }
-
             //load weather
             getWeatherData(response.city.coord.lat, response.city.coord.lon, response.city.name);
-
-        }).catch(function(response){
+        }).catch(function (response) {
             alert("Not a valid City");
         });
 }
 
-function showWeatherData(weatherData, city)
-{
+function showWeatherData(weatherData, city) {
     //load current
     var iconURL = "http://openweathermap.org/img/w/" + weatherData.current.weather[0].icon + ".png";  //get weather icon
-    $("#cityDate").html(city + " (" + new Date().toLocaleDateString() + ") <img id=\"icon\" src=\"" + iconURL  + "\" alt=\"Weather icon\"/>");
+    $("#cityDate").html(city + " (" + new Date().toLocaleDateString() + ") <img id=\"icon\" src=\"" + iconURL + "\" alt=\"Weather icon\"/>");
 
     var temp = parseInt(weatherData.current.temp);
-    temp = Math.round(((temp-273.15)*1.8) + 32);
-    $("#currentTemp").html(" " + temp +  "  &degF");
+    temp = Math.round(((temp - 273.15) * 1.8) + 32);
+    $("#currentTemp").html(" " + temp + "  &degF");
     $("#currentHumidity").html(weatherData.current.humidity + "%");
     $("#currentWindSpeed").html(weatherData.current.wind_speed + " MPH");
-
-   
 
     //load 5 Day
     var ul5 = $("#fiveDay");
     ul5.empty();
 
-    for (i=1; i < 6; i++)  //days 1-5
+    for (i = 1; i < 6; i++)  //days 1-5
     {
         //make the elements to display the 5 day forecast and append to the parent div
         var div = $("<div>").addClass("bg-primary");
 
-        var dateTime = parseInt(weatherData.daily[i].dt); 
+        var dateTime = parseInt(weatherData.daily[i].dt);
         var dateHeading = $("<h6>").text(new Date(dateTime * 1000).toLocaleDateString());  //convert unix time to javascript date
         var iconDayURL = "http://openweathermap.org/img/w/" + weatherData.daily[i].weather[0].icon + ".png";  //get weather icon
         var icon = $("<img>").attr("src", iconDayURL);
 
-        temp = parseInt(weatherData.daily[i].temp.day);  //convert kelvin to Fahrenheit
-        temp = Math.round(((temp-273.15)*1.8) + 32);  //convert kelvin to Fahrenheit
-        var temp5 = $("<p>").html("Temp: " + temp +  "  &degF");
+        //convert kelvin to Fahrenheit
+        temp = parseInt(weatherData.daily[i].temp.day);
+        temp = Math.round(((temp - 273.15) * 1.8) + 32);
+        var temp5 = $("<p>").html("Temp: " + temp + "  &degF");
 
         var humidity5 = $("<p>").html("Humidity: " + weatherData.daily[i].humidity + "%");
 
@@ -96,33 +83,30 @@ function showWeatherData(weatherData, city)
 }
 
 //load locations from local storage to the locations array
-function loadLocations()
-{
+function loadLocations() {
     var locationsArray = localStorage.getItem("locations");
     if (locationsArray) //if not undefined
     {
-      locations = JSON.parse(locationsArray);  //make sure there is a locations object in local storage
-      renderLocations();
+        locations = JSON.parse(locationsArray);  //make sure there is a locations object in local storage
+        renderLocations();
     }
     else {
-      localStorage.setItem("locations", JSON.stringify(locations));  //if not make one and store it to local storage
+        localStorage.setItem("locations", JSON.stringify(locations));  //if not make one and store it to local storage
     }
 }
 
-function renderLocations()
-{
+function renderLocations() {
     var divLocations = $("#locationHistory");
     divLocations.empty();  //clear the cities list before rendering it from the local storage object
 
-    $.each(locations, function(index, item){
+    $.each(locations, function (index, item) {
         var a = $("<a>").addClass("list-group-item list-group-item-action city").attr("data-city", locations[index]).text(locations[index]);
         divLocations.append(a);
     });
 
     $("#locationHistory > a").off();
 
-    $("#locationHistory > a").click(function (event)
-    {   
+    $("#locationHistory > a").click(function (event) {
         var element = event.target;
         var city = $(element).attr("data-city");
 
@@ -132,31 +116,29 @@ function renderLocations()
 }
 
 //save cities to the locations array and local storage
-function saveLocations(data)
-{
-    var city = data.city.name; 
+function saveLocations(data) {
+    var city = data.city.name;
 
     locations.unshift(city);
-    localStorage.setItem("locations", JSON.stringify(locations));  //convert to a string and sent to local storage
+    //convert to a string and sent to local storage
+    localStorage.setItem("locations", JSON.stringify(locations));
 }
 
 $(document).ready(function () {
-
     // hide the empty data containers
-    $("#weatherData").hide();  
+    $("#weatherData").hide();
 
     // display the city history
-    loadLocations();  
-
+    loadLocations();
 
     $("#searchBtn").click(function (event) {  //event handler for the city search input
-        var element = event.target; 
+        var element = event.target;
         var searchCriteria = $("#cityCode").val();  //get the user input
-        
+
         if (searchCriteria !== "")  //make sure it is not empty
         {
-            loadWeatherCity(searchCriteria, false);  
-            
+            loadWeatherCity(searchCriteria, false);
+
         }
     });
 });
